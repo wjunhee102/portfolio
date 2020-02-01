0d7ed3bf04c4ac215c2b9a325440de1e6c8db159
wjunhee102 <wjunhee102@gmail.com>
2020-02-02 03:06:58 +0900

    Updates

---
+++ precache-manifest.8daf368c6d02fcd3776b08ad7479d267.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a1376e3ac20e9569a99e7cd3d70e75",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c92ce07884635e5b5891",
+    "url": "/portfolio/static/css/main.47bd2791.chunk.css"
+  },
+  {
+    "revision": "fe7e2cab0773743a5557",
+    "url": "/portfolio/static/js/2.2df6c82d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.2df6c82d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c92ce07884635e5b5891",
+    "url": "/portfolio/static/js/main.62787b58.chunk.js"
+  },
+  {
+    "revision": "33455e316b53e5d7e6ee",
+    "url": "/portfolio/static/js/runtime-main.4adfa6bc.js"
+  }
+]);
